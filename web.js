db5d2b4c04d2a3a1b0122f032b85cf31d9521c2d
Connor Bond <connorbond@outlook.com>
2014-04-15 13:44:35 +0100

    Change render

--- web.js
+++ web.js
@@ -3,14 +3,25 @@ var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
 
+app.configure(function(){
+
+  // disable layout
+  app.set("view options", {layout: false});
+
+  // make a custom html template
+  app.register('.html', {
+    compile: function(str, options){
+      return function(locals){
+        return str;
+      };
+    }
+  });
+});
+
 app.use(logfmt.requestLogger());
 
 app.set('views', __dirname + '/views');
-app.engine('html', require('ejs').renderFile);
-
-/*app.get('/', function(req, res) {
-  res.send('Have some Computerlove!');
-});*/
+//app.engine('html', require('ejs').renderFile);
 
 app.get('/', function (req, res)
 {
