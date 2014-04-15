b5915938812d40ab8c32d86ec87a9c100208d27e
Connor Bond <connorbond@outlook.com>
2014-04-15 15:52:18 +0100

    Express

--- web.js
+++ web.js
@@ -2,33 +2,18 @@
 var express = require("express");
 var logfmt = require("logfmt");
 var app = express();
-
-app.configure(function(){
-
-  // disable layout
-  app.set("view options", {layout: false});
-
-  // make a custom html template
-  app.register('.html', {
-    compile: function(str, options){
-      return function(locals){
-        return str;
-      };
-    }
-  });
-});
+var port = Number(process.env.PORT || 5000);
 
 app.use(logfmt.requestLogger());
 
 app.set('views', __dirname + '/views');
-//app.engine('html', require('ejs').renderFile);
+app.engine('html', require('ejs').renderFile);
 
 app.get('/', function (req, res)
 {
     res.render('index.html');
 });
 
-var port = Number(process.env.PORT || 5000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
