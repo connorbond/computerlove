0bfa4cabb595109b20d1d2e9abf7c496f0cc9fb1
Connor Bond <connorbond@outlook.com>
2014-04-17 09:37:15 +0100

    Different Port

--- app.js
+++ app.js
@@ -34,4 +34,9 @@ app.get('/', function (req, res) {
   )
 })*/
 
-app.listen(3000)
+//app.listen(3000)
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
